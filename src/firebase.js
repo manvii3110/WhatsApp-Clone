cdd021bc7027a9c33918e4a19d6b1965cd837f05
Manvi <msinghal3102@gmail.com>
2021-09-01 15:56:35 +0530

    Added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBCjtmcYFjRgETVOii3_J_QedSHwExERYI",
+  authDomain: "whatsapp-clone-c34ca.firebaseapp.com",
+  projectId: "whatsapp-clone-c34ca",
+  storageBucket: "whatsapp-clone-c34ca.appspot.com",
+  messagingSenderId: "682079412396",
+  appId: "1:682079412396:web:7525aa8727f839b0be3f09",
+  measurementId: "G-P2J92TJY2V"
+};
